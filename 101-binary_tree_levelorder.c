aa87bf09c97aaf94a5e647d2f1e7050fca65b210
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2023-11-02 03:29:22 -0700

    task 20 mod

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -25,7 +25,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
  * @level: level of the tree to perform a function on
  * @func: function to perform
  */
-void binary_tree_level(const binary_tree_t *tree, size_t level, void (*func)(int))
+void binary_tree_level(const binary_tree_t *tree, size_t level,
+		void (*func)(int))
 {
 	if (tree == NULL)
 		return;
